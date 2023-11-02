0a4fc2a1da6dfe382d88456311406aedbe2428ea
Thaker13 <149239169+Thaker13@users.noreply.github.com>
2023-11-02 00:21:23 +0000

    Add files via upload

---
+++ CharacterDataType.java
@@ -0,0 +1,18 @@
+package characterdatatype;
+
+public class CharacterDataType {
+
+    public static void main ( String [] args){
+
+        char A = 10;
+        char B = 20;
+
+        System.out.println ( A + B );
+        System.out.println ( A - B );
+        System.out.println ( A * B );
+        System.out.println ( A / B );
+        System.out.println ( A % B );
+
+
+    }
+}
